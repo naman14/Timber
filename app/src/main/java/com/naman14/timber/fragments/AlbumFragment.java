5ebb9a8210cdfdb85088088b617ec74806e87480
naman14 <namandwivedi14@gmail.com>
2015-07-22 13:10:52 +0530

    Revert "revert moving to fragments commit"
    This reverts commit 192c2245c185abb5d26af8f96d063452008ed2c1.

--- app/src/main/java/com/naman14/timber/fragments/AlbumFragment.java
+++ app/src/main/java/com/naman14/timber/fragments/AlbumFragment.java
@@ -22,7 +22,7 @@ public class AlbumFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(
-                R.layout.fragment_main, container, false);
+                R.layout.fragment_recyclerview, container, false);
 
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 

883e911587c1f8a276468bee8c656c0cee7bb9fe
naman14 <namandwivedi14@gmail.com>
2015-08-07 19:24:29 +0530

    circular seek bar

---
+++ app/src/main/java/com/naman14/timber/activities/SettingsActivity.java
@@ -0,0 +1,9 @@
+package com.naman14.timber.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by naman on 07/08/15.
+ */
+public class SettingsActivity extends AppCompatActivity {
+}

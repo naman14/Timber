8292094eb352537172bd718d3d7994bf544f63a8
naman14 <namandwivedi14@gmail.com>
2015-09-29 19:36:29 +0530

    relaunch app on theme change

--- app/src/main/java/com/naman14/timber/fragments/SettingsFragment.java
+++ app/src/main/java/com/naman14/timber/fragments/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
@@ -21,12 +22,14 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
     private static final String NOW_PLAYING_SELECTOR="now_paying_selector";
     private static final String KEY_ABOUT="preference_about";
     private static final String KEY_SOURCE="preference_source";
+    private static final String KEY_THEME = "theme_preference";
     private static final String TOGGLE_ANIMATIONS="toggle_animations";
     private static final String TOGGLE_SYSTEM_ANIMATIONS="toggle_system_animations";
 
     Preference nowPlayingSelector;
     Preference about, source;
     SwitchPreference toggleAnimations;
+    ListPreference themePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         nowPlayingSelector = findPreference(NOW_PLAYING_SELECTOR);
         about = findPreference(KEY_ABOUT);
         source = findPreference(KEY_SOURCE);
+        themePreference = (ListPreference) findPreference(KEY_THEME);
         toggleAnimations=(SwitchPreference)findPreference(TOGGLE_ANIMATIONS);
 
         nowPlayingSelector.setIntent(NavigationUtils.getNavigateToStyleSelectorIntent(getActivity(), Constants.SETTINGS_STYLE_SELECTOR_NOWPLAYING));
@@ -65,6 +69,15 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
                 intent.setData(Uri.parse("https:github.com/naman14/Timber"));
                 startActivity(intent);
                 return false;
+            }
+        });
+        themePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Intent i = getActivity().getBaseContext().getPackageManager().getLaunchIntentForPackage( getActivity().getBaseContext().getPackageName() );
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                return true;
             }
         });
     }

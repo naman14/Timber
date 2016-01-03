64c1d310324263674d764763b3569550d1837744
naman14 <namandwivedi14@gmail.com>
2016-01-03 15:39:50 +0530

    derp

--- app/src/main/java/com/naman14/timber/activities/MainActivity.java
+++ app/src/main/java/com/naman14/timber/activities/MainActivity.java
@@ -319,7 +319,6 @@ public class MainActivity extends BaseActivity implements ATEActivityThemeCustom
             navigationView.getMenu().findItem(R.id.nav_library).setChecked(true);
             Fragment fragment = new MainFragment();
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.hide(getSupportFragmentManager().findFragmentById(R.id.fragment_container));
             transaction.replace(R.id.fragment_container, fragment).commitAllowingStateLoss();
 
         }

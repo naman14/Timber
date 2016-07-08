1478b6a1e6ee97dd7f96f8dc726daf6c28d90084
nv95 <nvasya95@gmail.com>
2016-07-08 10:45:52 +0200

    upgrade gradle version

--- app/src/main/java/com/naman14/timber/activities/NowPlayingActivity.java
+++ app/src/main/java/com/naman14/timber/activities/NowPlayingActivity.java
@@ -49,7 +49,6 @@ public class NowPlayingActivity extends BaseActivity implements ATEActivityTheme
         return Config.LIGHT_TOOLBAR_AUTO;
     }
 
-    @Override
     public int getToolbarColor() {
         return Color.TRANSPARENT;
     }

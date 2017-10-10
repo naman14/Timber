9586d31673a0fde330b2ba73519469eb5228d228
shailandraRajput <shailandrarajput1234.sr@gmail.com>
2017-10-10 09:01:31 -0700

    Download Mising album art

--- app/src/main/java/com/naman14/timber/utils/ImageUtils.java
+++ app/src/main/java/com/naman14/timber/utils/ImageUtils.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2015 Naman Dwivedi
- *
- * Licensed under the GNU General Public License v3
- *
- * This is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- */
-
 package com.naman14.timber.utils;
 
 import android.content.Context;
@@ -20,11 +6,77 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v8.renderscript.RenderScript;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.naman14.timber.R;
+import com.naman14.timber.dataloaders.AlbumLoader;
+import com.naman14.timber.lastfmapi.LastFmClient;
+import com.naman14.timber.lastfmapi.callbacks.AlbuminfoListener;
+import com.naman14.timber.lastfmapi.models.AlbumQuery;
+import com.naman14.timber.lastfmapi.models.LastfmAlbum;
+import com.naman14.timber.models.Album;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 public class ImageUtils {
+    private static final DisplayImageOptions lastfmDisplayImageOptions =
+            new DisplayImageOptions.Builder()
+                    .cacheInMemory(true)
+                    .cacheOnDisk(true)
+                    .showImageOnFail(R.drawable.ic_empty_music2)
+                    .build();
+
+    private static final DisplayImageOptions diskDisplayImageOptions =
+            new DisplayImageOptions.Builder()
+                    .cacheInMemory(true)
+                    .build();
+
+    public static void loadAlbumArtIntoView(final long albumId, final ImageView view) {
+        loadAlbumArtIntoView(albumId, view, new SimpleImageLoadingListener());
+    }
+
+    public static void loadAlbumArtIntoView(final long albumId, final ImageView view,
+                                            final ImageLoadingListener listener) {
+        ImageLoader.getInstance()
+                .displayImage(TimberUtils.getAlbumArtUri(albumId).toString(),
+                        view,
+                        diskDisplayImageOptions,
+                        new SimpleImageLoadingListener() {
+                            @Override
+                            public void onLoadingFailed(String imageUri, View view,
+                                                        FailReason failReason) {
+                                loadAlbumArtFromLastfm(albumId, (ImageView) view);
+                                listener.onLoadingFailed(imageUri, view, failReason);
+                            }
+                        });
+    }
+
+    private static void loadAlbumArtFromLastfm(long albumId, final ImageView albumArt) {
+        Album album = AlbumLoader.getAlbum(albumArt.getContext(), albumId);
+        LastFmClient.getInstance(albumArt.getContext())
+                .getAlbumInfo(new AlbumQuery(album.title, album.artistName),
+                        new AlbuminfoListener() {
+                            @Override
+                            public void albumInfoSucess(final LastfmAlbum album) {
+                                if (album != null) {
+                                    ImageLoader.getInstance()
+                                            .displayImage(album.mArtwork.get(4).mUrl,
+                                                    albumArt,
+                                                    lastfmDisplayImageOptions);
+                                }
+                            }
+
+                            @Override
+                            public void albumInfoFailed() { }
+                        });
+    }
 
     public static Drawable createBlurredImageFromBitmap(Bitmap bitmap, Context context, int inSampleSize) {
 
@@ -48,5 +100,4 @@ public class ImageUtils {
 
         return new BitmapDrawable(context.getResources(), blurTemplate);
     }
-
 }

a67f851c7ba10ed3ca69fc31198b66941dbfd66a
shailandraRajput <shailandrarajput1234.sr@gmail.com>
2017-10-10 09:00:19 -0700

    Download Mising Album art

--- app/src/main/java/com/naman14/timber/lastfmapi/models/LastfmAlbum.java
+++ app/src/main/java/com/naman14/timber/lastfmapi/models/LastfmAlbum.java
@@ -1,18 +1,14 @@
-/*
- * Copyright (C) 2015 Naman Dwivedi
- *
- * Licensed under the GNU General Public License v3
- *
- * This is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- */
-
 package com.naman14.timber.lastfmapi.models;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class LastfmAlbum {
+    private static final String IMAGE = "image";
+
+    @SerializedName(IMAGE)
+    public List<Artwork> mArtwork;
+
+    // Only needed fields have been defined. See https://www.last.fm/api/show/album.getInfo
 }

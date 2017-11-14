b23122c741888650d9d2fe37047961090dffc596
Cedric Laier <Rintel@users.noreply.github.com>
2017-11-14 18:52:18 +0100

    configure build.gradle to enable unit testing and add an empty test

---
+++ app/src/androidTest/java/com/naman14/timber/activities/SettingsActivityTest.java
@@ -0,0 +1,27 @@
+package com.naman14.timber.activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by CedricsPC on 14.11.2017.
+ */
+public class SettingsActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+    }
+
+    @Test
+    public void getActivityTheme() throws Exception {
+    }
+
+    @Test
+    public void onColorSelection() throws Exception {
+    }
+
+}

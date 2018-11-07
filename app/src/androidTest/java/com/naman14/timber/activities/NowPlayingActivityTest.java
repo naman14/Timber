fcbc34de3d8c703d26dee304c6a891217db82a39
Luis Abularach <luisabularach48@gmail.com>
2018-11-06 22:41:28 -0800

    added test to test search history

---
+++ app/src/androidTest/java/com/naman14/timber/activities/NowPlayingActivityTest.java
@@ -0,0 +1,45 @@
+package com.naman14.timber.activities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class NowPlayingActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void getActivityTheme() {
+    }
+
+    @Test
+    public void getLightToolbarMode() {
+    }
+
+    @Test
+    public void getLightStatusBarMode() {
+    }
+
+    @Test
+    public void getToolbarColor() {
+    }
+
+    @Test
+    public void getStatusBarColor() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+}

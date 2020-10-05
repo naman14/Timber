bc653adc7209f1771dd4312968b8f41579902a8a
Naman Dwivedi <namandwivedi14@gmail.com>
2020-10-06 00:18:20 +0530

    update about dialog

--- app/src/main/java/com/naman14/timber/utils/Helpers.java
+++ app/src/main/java/com/naman14/timber/utils/Helpers.java
@@ -29,11 +29,14 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.naman14.timber.R;
 
 public class Helpers {
@@ -57,96 +60,27 @@ public class Helpers {
 
     public static class AboutDialog extends DialogFragment {
 
-        String urlgooglelus = "https://plus.google.com/u/0/+NamanDwivedi14";
-        String urlcommunity = "https://plus.google.com/communities/111029425713454201429";
-        String urltwitter = "https://twitter.com/naman1405";
-        String urlgithub = "https://github.com/naman14";
-        String urlsource = "https://github.com/naman14/Timber/issues";
-
         public AboutDialog() {
         }
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-            LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(
-                    Context.LAYOUT_INFLATER_SERVICE);
-            LinearLayout aboutBodyView = (LinearLayout) layoutInflater.inflate(R.layout.layout_about_dialog, null);
-
-            TextView appversion = (TextView) aboutBodyView.findViewById(R.id.app_version_name);
-
-            TextView googleplus = (TextView) aboutBodyView.findViewById(R.id.googleplus);
-            TextView twitter = (TextView) aboutBodyView.findViewById(R.id.twitter);
-            TextView github = (TextView) aboutBodyView.findViewById(R.id.github);
-            TextView source = (TextView) aboutBodyView.findViewById(R.id.source);
-            TextView community = (TextView) aboutBodyView.findViewById(R.id.feature_request);
-
-            TextView dismiss = (TextView) aboutBodyView.findViewById(R.id.dismiss_dialog);
-            dismiss.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dismiss();
-                }
-            });
-            googleplus.setPaintFlags(googleplus.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-            twitter.setPaintFlags(twitter.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-            github.setPaintFlags(github.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-
-            googleplus.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlgooglelus));
-                    startActivity(i);
-                }
-
-            });
-            twitter.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urltwitter));
-                    startActivity(i);
-                }
-
-            });
-            github.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlgithub));
-                    startActivity(i);
-                }
-
-            });
-            source.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlsource));
-                    startActivity(i);
-                }
-            });
-            community.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlcommunity));
-                    startActivity(i);
-                }
-            });
+            String appName = "Timber";
             try {
                 PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
                 String version = pInfo.versionName;
                 int versionCode = pInfo.versionCode;
-                appversion.setText("Timber " + version);
+                appName = "Timber " + version;
             } catch (PackageManager.NameNotFoundException e) {
                 e.printStackTrace();
             }
 
-            return new AlertDialog.Builder(getActivity())
-                    .setView(aboutBodyView)
-                    .create();
+            return new MaterialDialog.Builder(getActivity())
+                    .title(appName)
+                    .content(Html.fromHtml(getString(R.string.about_dialog_body)))
+                    .positiveText("Dismiss")
+                    .build();
         }
 
     }
